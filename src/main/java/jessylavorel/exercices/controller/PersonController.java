0d25b1ad1ac8a19c4fe828e56be922fefa4da9a9
Jessy Lavorel (20008590) <jessy.lavorel@ext.adeo.com>
2024-10-10 12:48:43 +0200

    feat: stream and controller

--- src/main/java/jessylavorel/exercices/controller/PersonController.java
+++ src/main/java/jessylavorel/exercices/controller/PersonController.java
@@ -1,10 +1,6 @@
 package jessylavorel.exercices.controller;
 
 import jessylavorel.exercices.dto.Person;
-import jessylavorel.exercices.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
