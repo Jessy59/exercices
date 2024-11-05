5bd42870d9b19a3246b0c1c6dc9ce9d224861ea2
Jessy Lavorel (20008590) <jessy.lavorel@ext.adeo.com>
2024-11-05 13:52:28 +0100

    feat: fizzbuzz

--- src/main/java/jessylavorel/exercices/service/FizzBuzz.java
+++ src/main/java/jessylavorel/exercices/service/FizzBuzz.java
@@ -1,10 +1,34 @@
 package jessylavorel.exercices.service;
 
+import java.util.stream.IntStream;
+
 /**
  * @author 20008590
  */
 public class FizzBuzz {
     public static void main(String[] args) {
 
+        // Using for loop
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+
+        }
+
+        // Using stream api
+        IntStream.rangeClosed(1, 100).
+                mapToObj(i -> {
+                    if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+                    else if (i % 3 == 0) return "Fizz";
+                    else if (i % 5 == 0) return "Buzz";
+                    else return i;
+                })
+                .forEach(System.out::println);
     }
 }
